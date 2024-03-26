7a322be45232ff7cf21b1310d1ccdb51503ae742
dalyn <dalin114466@gmail.com>
2024-03-26 13:54:42 +0700

    laravel breezz

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-//Client side
 Route::get('/', function () {
-    return view('clientSide/pages/homepage');
+    return view('/pages/homePage');
 });
 
-//ServerSide
-Route::get('/login', function () {
-    return view('serverSide/auth/login');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/signup', function () {
-    return view('serverSide/auth/signup');
-});
+
+require __DIR__.'/auth.php';
