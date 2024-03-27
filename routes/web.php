36f15d6f94584a004fb02345a3adb1c7c370e2c9
dalyn <dalin114466@gmail.com>
2024-03-27 15:51:52 +0700

    product crud

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -28,6 +30,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::post('auth', [AuthenticatedSessionController::class, 'destroy'])->name('auth.logout');
+
     
     // Our resource routes
     Route::resource('roles', RoleController::class);
