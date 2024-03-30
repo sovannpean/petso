7b304212c6b3006828ef5df4740a1fe48ffe3def
MacOS <macos@MacOSs-MacBook-Pro-4.local>
2024-03-29 19:12:59 -0700

    Home Page

---
+++ resources/views/components/top-nav.blade.php
@@ -0,0 +1,19 @@
+<div class="bg-[#5B96A6]">
+    <div class="container w-xl h-[50px] mx-auto flex justify-between">
+        <div class="flex gap-5">
+            <div class="flex items-center gap-3 text-white">
+                <i class="fa-solid fa-phone text-lg"></i>
+                <p>014 418 234</p>
+            </div>
+            <div class="flex items-center gap-3 text-white">
+                <i class="fa-solid fa-envelope text-lg"></i>
+                <p>petso@gmail.com</p>
+            </div>
+        </div>
+        <div class="flex gap-5 items-center">
+            <a href="#"><i class="fa-brands fa-facebook text-xl text-white hover:text-[#6CC1D8]"></i></a>
+            <a href="#"><i class="fa-brands fa-telegram text-xl text-white hover:text-[#6CC1D8]"></i></a>
+            <a href="#"><i class="fa-brands fa-square-instagram text-xl text-white hover:text-[#6CC1D8]"></i></a>
+        </div>
+    </div>
+</div>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,24 +7,36 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        {{-- Icon Link --}}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
     <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
-                <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-                </a>
-            </div>
 
-            <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {{ $slot }}
-            </div>
-        </div>
+        {{-- Navbar --}}
+        <section>
+            <x-top-nav/>
+            <x-nav-search/>
+            <x-nav-menu/>
+        </section>
+
+        {{-- Content Page --}}
+        <section>
+            {{ $slot }}
+        </section>
+
+        {{-- Footer --}}
+        <section>
+
+        </section>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
     </body>
 </html>
