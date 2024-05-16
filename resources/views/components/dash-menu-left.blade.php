a19b4c91552623edccc0016236e7dff773711fc2
dalyn <dalin114466@gmail.com>
2024-05-16 23:40:22 +0700

    change the category

--- resources/views/components/dash-menu-left.blade.php
+++ resources/views/components/dash-menu-left.blade.php
@@ -89,7 +89,7 @@
                 <ul id="dropdown-sales" class="hidden py-2 space-y-2">
                     <li>
                         <a href="#"
-                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Donation</a>
+                            class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Manage User</a>
                     </li>
                     <li>
                         <a href="#"
--- routes/auth.php
+++ routes/auth.php
@@ -10,20 +10,17 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-
 
 Route::middleware('guest')->group(function () {
-
-    Route::get('register', [AuthController::class, 'showRegisterForm'])
+    Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-    Route::post('register', [AuthController::class, 'register']);
+    Route::post('register', [RegisteredUserController::class, 'store']);
 
-    Route::get('login', [AuthController::class, 'showLoginForm'])
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
-    Route::post('login', [AuthController::class, 'login']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
                 ->name('password.request');
