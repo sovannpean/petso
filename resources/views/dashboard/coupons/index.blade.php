5a2781ab98ab72342347b2142cca180dd1725317
MacOS <macos@MacOSs-MacBook-Pro-4.local>
2024-05-19 16:51:08 -0700

    update dashboard UI

--- resources/views/dashboard/coupons/index.blade.php
+++ resources/views/dashboard/coupons/index.blade.php
@@ -30,28 +30,102 @@
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($coupons as $coupon)
-                        <tr>
-                            <td>{{ $coupon->code }}</td>
-                            <td>{{ $coupon->name }}</td>
-                            <td>{{ $coupon->discount_amount }}%</td>
-                            <td>{{ $coupon->status ? 'Active' : 'Inactive' }}</td>
-                            <td>{{ $coupon->starts_at->format('Y-m-d H:i') }}</td>
-                            <td>{{ $coupon->expires_at->format('Y-m-d H:i') }}</td>
-                            <td>
-                                <form action="{{ route('coupons.apply') }}" method="POST">
-                                    @csrf
-                                    <input type="hidden" name="coupon_code" value="{{ $coupon->code }}">
-                                    <select name="product_id" required>
-                                        <option value="" disabled selected>Select a product</option>
-                                        @foreach ($products as $product)
-                                            <option value="{{ $product->id }}">{{ $product->name }} - ${{ $product->price }}</option>
-                                        @endforeach
-                                    </select>
-                                    <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                                        Apply
-                                    </button>
-                                </form>
+                    @foreach($products as $item)
+                        <tr class="border-b dark:border-gray-600 hover:bg-gray-100 dark:hover:bg-gray-700">
+                            {{-- Id --}}
+                            <td scope="row" class="w-4 px-4 py-3 text-center font-medium text-gray-900 whitespace-nowrap">
+                                {{ $loop->iteration }}
+                            </td>
+
+                            {{-- Name --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap">
+                                {{ $item->name }}
+                            </td>
+
+                            {{-- Image --}}
+                            <td class="flex items-center justify-center px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <img src="{{ asset('/images/' . $item->images) }}" height="50" width="50" onchange="showFile(event)" required>
+                            </td>
+
+                            {{-- Price --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                ${{ number_format((float)$item->price, 2) }}
+                            </td>
+
+                            {{-- Size --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $item->size }}
+                            </td>
+
+                            {{-- Weight --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $item->weight }}
+                            </td>
+
+                            {{-- Detail --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $item->detail }}
+                            </td>
+
+                            {{-- Rating --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                <div class="flex items-center justify-center">
+                                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    <svg aria-hidden="true" class="w-5 h-5 text-yellow-400" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                    </svg>
+                                    <span class="ml-1 text-gray-500 dark:text-gray-400">5.0</span>
+                                </div>
+                            </td>
+
+                            {{-- Categories --}}
+                            <td class="px-4 py-2 text-center font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $item->category->name }}
+                            </td>
+
+                            {{-- Actions --}}
+                            <td class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap dark:text-white text-center">
+                                <a href="{{ url('/dashboard/products/show', $item->id)}}" title="View Item" class="btn btn-primary btn-sm">
+                                    <i class="fa fa-eye" aria-hidden="true"></i> View
+                                </a>
+                            </td>
+                            
+                            {{-- Edit and Delete --}}
+                            <td class="px-4 py-2 text-center">
+                                <button id="{{ $loop->iteration }}" data-dropdown-toggle="{{ $item->name }}" class="inline-flex items-center text-sm font-medium text-center text-gray-500 hover:text-gray-800 rounded-lg focus:outline-none dark:text-gray-400 dark:hover:text-gray-100" type="button">
+                                    <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z" />
+                                    </svg>
+                                </button>
+                                <div id="{{ $item->name }}" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
+                                    <ul class="pt-4 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="{{ $loop->iteration }}">
+                                        <li>
+                                            <form method="POST" action="{{ url('/dashboard/products/index/' . $item->id)}}" accept-charset="UTF-8" style="display:inline" class="block py-4 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                {{ csrf_field() }}
+                                                <button type="submit" class="w-full text-start" title="Delete" onclick="return confirm(&quot;Confirm delete?&quot;)">
+                                                    <i class="fa fa-trash" class="fa fa-trash-o" aria-hidden="true"></i> Delete
+                                                </button>
+                                            </form>
+                                        </li>
+                                        <li>
+                                            <a href="{{ url('/dashboard/products/update', $item->id)}}" title="Edit Item" class="block text-start py-4 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                                                <i class="fa-solid fa-pen-to-square" aria-hidden="true"></i>
+                                                Edit
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </td>
                         </tr>
                     @endforeach
