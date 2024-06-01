014e2c617342c862a704296dcfa2f55bd89f7451
MacOS <macos@MacOSs-MacBook-Pro-4.local>
2024-05-31 19:04:25 -0700

    update

--- resources/views/pages/orderPage.blade.php
+++ resources/views/pages/orderPage.blade.php
@@ -75,31 +75,75 @@
                             <div>
                                 <h1 class="text-lg font-medium">{{ $product->name }}</h1>
                                 <p class="text-sm text-gray-800 font-bold">Quantity: {{ $cart[$product->id] }}</p>
-                                <p class="text-sm text-gray-500">Ship every 3 weeks</p>
                             </div>
                         </div>
                         <h1 class="text-sm">${{ $product->price * $cart[$product->id] }}</h1>
                     </div>
                 @endforeach
 
-                <div class="mt-5 border-b border-gray-500 pb-5">
-                    <div class="flex justify-between">
+                <div class="border border-gray-300 p-5 mt-5">
+
+                    <div class="flex justify-between border-b border-gray-300 pb-4">
                         <p>Subtotal</p>
                         <h1 class="text-sm">
                             ${{ collect($selectedProducts)->sum(function($product) use ($cart) { return $product->price * $cart[$product->id]; }) }}
                         </h1>
                     </div>
-                    <div class="flex justify-between mt-5">
-                        <p>Shipping</p>
-                        <p>Calculated at next step</p>
+
+                    <p class="mt-5">Delivery Method</p>
+                    <div class="flex items-center justify-between mb-4 bg-blue-300 py-2 px-4 rounded-md mt-5 text-gray-800">
+                        <div>
+                            <i class="fa-solid fa-truck"></i>
+                            <label for="default-radio-1" class="ms-2 text-sm font-medium dark:text-gray-300">Deliver in 8-36 Hours:</label>
+                        </div>
+                        <div class="flex items-center gap-2">
+                            <span>$1.50</span>
+                            <input id="default-radio-1" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                        </div>
                     </div>
+
+                    <div class="flex items-center justify-between mb-4 bg-yellow-100 py-2 px-4 rounded-md">
+                        <div>
+                            <label for="default-radio-2" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Pick up at shop</label>
+                        </div>
+                        <input checked id="default-radio-2" type="radio" value="" name="default-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                    </div>
+
+                    <div class="mt-5 border-t border-gray-300 pt-4">
+                        <div class="flex justify-between">
+                            <p class="text-xl font-semibold">Total</p>
+                            <h1 class="text-lg font-semibold">
+                                ${{ collect($selectedProducts)->sum(function($product) use ($cart) { return $product->price * $cart[$product->id]; }) }}
+                            </h1>
+                        </div>
+                    </div>
+                    
                 </div>
-                <div class="mt-5 pb-5">
-                    <div class="flex justify-between">
-                        <p class="text-xl font-semibold">Total</p>
-                        <h1 class="text-lg font-semibold">
-                            ${{ collect($selectedProducts)->sum(function($product) use ($cart) { return $product->price * $cart[$product->id]; }) }}
-                        </h1>
+
+                <div class="border border-gray-300 p-5 mt-5">
+                    <div id="accordion-flush" data-accordion="collapse" data-active-classes="dark:bg-gray-900 text-gray-900 dark:text-white" data-inactive-classes="text-gray-500 dark:text-gray-400">
+                        <h2 id="accordion-flush-heading-2">
+                            <button type="button" class="w-full py-5 text-start font-medium rtl:text-right text-gray-800 border-b border-gray-300 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-2" aria-expanded="false" aria-controls="accordion-flush-body-2">
+                                <input checked type="radio" value="" name="pay-vai" class="w-4 h-4 text-blue-600 bg-gray-600 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <span>Transfer to (ABA, Acleda, TrueMoney or Wing)</span>
+                            </button>
+                        </h2>
+                        <div id="accordion-flush-body-2" class="hidden" aria-labelledby="accordion-flush-heading-2">
+                            <div class="py-5 border-b border-gray-200 dark:border-gray-700">
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">Make your payment into our bank account or Phone Number (ABA, Acleda, Wing, True Money, Pi Pay or Weiluy). Please use your Order ID as the payment reference. Bank details provided when you click “Place Order”.</p>
+                            </div>
+                        </div>
+                        <h2 id="accordion-flush-heading-1">
+                            <button type="button" class="w-full text-start py-5 font-medium rtl:text-right text-gray-800 border-b border-gray-300 dark:border-gray-700 dark:text-gray-400 gap-3" data-accordion-target="#accordion-flush-body-1" aria-expanded="true" aria-controls="accordion-flush-body-1">
+                                <input checked type="radio" value="" name="pay-vai" class="w-4 h-4 text-blue-600 bg-gray-600 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                <span>ABA / Cash on Delivery</span>
+                            </button>
+                        </h2>
+                        <div id="accordion-flush-body-1" class="hidden" aria-labelledby="accordion-flush-heading-1">
+                            <div class="py-5 border-b border-gray-300 dark:border-gray-700">
+                                <p class="mb-2 text-gray-500 dark:text-gray-400">Pay Cash / ABA to the deliveryman when you receive your package. Please keep enough changes ready.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
